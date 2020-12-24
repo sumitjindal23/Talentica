da4cf57a04df0ff8d896f7682c40531d8c42aaa6
sumitjindal23 <sumitjindal2010@gmail.com>
2020-12-24 11:37:46 +0530

    Update Main.java

--- InvoiceProject/InvoiceGenerator/src/com/company/Main.java
+++ InvoiceProject/InvoiceGenerator/src/com/company/Main.java
@@ -31,20 +31,20 @@ public class Main {
             String vat = "0%";
             String addTax = "0%";
             Double grossPrice = Double.parseDouble(price) * Integer.parseInt(qty);
-            Double tax = 0.0;
+            Double cost =grossPrice;
             if (isVat && isImport) {
-                tax = 12.5 + 2.4;
+                cost = getCostAfterTax(cost, 12.5);
+                cost = getCostAfterTax(cost, 2.4);
                 vat = "12.5%";
                 addTax = "2.4%";
             } else if (isVat && !isImport) {
-                tax = 12.5;
+                cost = getCostAfterTax(cost, 12.5);
                 vat = "12.5%";
+
             } else if (!isVat && isImport) {
-                tax = 2.4;
+                cost = getCostAfterTax(cost, 2.4);
                 addTax = "2.4%";
             }
-
-            Double cost = Math.round((grossPrice + grossPrice * (tax) / 100) * 100.0) / 100.0;
             total = total + cost;
 
             invoiceRecords.append(CSVUtility.writeCsvRecords(
@@ -54,6 +54,10 @@ public class Main {
         invoiceRecords.append("Total: " + Math.round(total * 100.0) / 100.0);
         System.out.println(invoiceHeader + invoiceRecords);
 
+    }
+
+    private static double getCostAfterTax(Double cost, double tax) {
+        return Math.round((cost + cost * (tax) / 100) * 100.0) / 100.0;
     }
 
     public static boolean stringContainsItemFromList(String inputStr, String[] items) {
